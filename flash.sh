590c94d71c4d71263f40c94223f88c9180499d2c
zwiebert <bertw@online.de>
2018-09-06 19:35:00 +0200

    new binaries

--- flash.sh
+++ flash.sh
@@ -10,6 +10,6 @@ then
     echo "usage: . flash.sh /dev/ttyUSBx"  
 else
 
-python tools/esptool.py -p "$comport"  write_flash -ff 40m -fm dio -fs 4MB 0x00000 firmware/eagle.flash.bin 0x10000 firmware/eagle.irom0text.bin  0x3FC000 firmware/esp_init_data_default.bin 0x3FE000 firmware/blank.bin
+python tools/esptool.py -p "$comport"  write_flash -ff 40m -fm dio -fs 4MB 0x00000 firmware/eagle.flash.bin 0x10000 firmware/eagle.irom0text.bin  0x3FC000 firmware/esp_init_data_default_v08.bin 0x3FE000 firmware/blank.bin
 
 fi
