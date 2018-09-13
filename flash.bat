913592c726452f38c1a22859b41c6c3494291d7f
zwiebert <bertw@online.de>
2018-09-13 23:07:35 +0200

    new binaries

--- flash.bat
+++ flash.bat
@@ -3,5 +3,5 @@
 
 set comport=%1
 
-tools\esptool.exe -p "%comport%" write_flash -ff 40m -fm dio -fs 4MB 0x00000 firmware/eagle.flash.bin 0x10000 firmware/eagle.irom0text.bin  0x3FC000 firmware/esp_init_data_default_v08.bin 0x3FE000 firmware/blank.bin
+tools\esptool.exe -p "%comport%" write_flash -ff 40m -fm dio -fs 4MB 0x00000 firmware/esp8266/eagle.flash.bin 0x10000 firmware/esp8266/eagle.irom0text.bin  0x3FC000 firmware/esp8266/esp_init_data_default_v08.bin 0x3FE000 firmware/esp8266/blank.bin
 
