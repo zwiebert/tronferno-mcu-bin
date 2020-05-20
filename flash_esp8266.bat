e6d1716b391b797f8f1d5a5120641ce1e39b80ff
zwiebert <bertw@online.de>
2020-05-20 22:47:17 +0200

    Remove esp8266 binaries

--- flash_esp8266.bat
+++
@@ -1,7 +0,0 @@
-@echo usage:  flash.bat COMx
-
-
-set comport=%1
-
-tools\esptool.exe --chip esp8266 -p "%comport%" write_flash -ff 40m -fm dio -fs 4MB 0x00000 firmware/esp8266/eagle.flash.bin 0x10000 firmware/esp8266/eagle.irom0text.bin  0x3FC000 firmware/esp8266/esp_init_data_default_v08.bin 0x3FE000 firmware/esp8266/blank.bin
-
