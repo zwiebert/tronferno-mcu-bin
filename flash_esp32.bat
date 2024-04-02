f291307c6db84591eb1778c291ca91cc842f1058
Bert Winkelmann <31831671+zwiebert@users.noreply.github.com>
2024-04-02 19:27:06 +0200

    Use espressif/esptool-4.7.0-win64 distribution files

--- flash_esp32.bat
+++ flash_esp32.bat
@@ -1,3 +1,3 @@
 set comport=%1
 
-tools\dist\esptool\esptool.exe --chip esp32 --port "%comport%" --baud 230400 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 firmware/esp32/bootloader.bin 0x100000 firmware/esp32/tronferno-mcu.bin 0x8000 firmware/esp32/partitions.bin 0x10000 firmware/esp32/ota_data_initial.bin
+tools\esptool-win64\esptool.exe --chip esp32 --port "%comport%" --baud 230400 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 firmware/esp32/bootloader.bin 0x100000 firmware/esp32/tronferno-mcu.bin 0x8000 firmware/esp32/partitions.bin 0x10000 firmware/esp32/ota_data_initial.bin
