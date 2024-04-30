380ad4d6d8b24ab97de8fa9b776f21f72b54f293
zwiebert <bertw@online.de>
2024-04-30 09:57:03 +0200

    Improve code

--- flash_esp32.sh
+++ flash_esp32.sh
@@ -9,7 +9,7 @@ fi
 esptool_bin="tools/esptool-linux-amd64/esptool"
 esptool_py="python tools/esptool.py"
 esptool="echo \nERROR: ESPTOOL NOT SET\n\n"
-esptool_runs="TBD"
+esptool_py_runs="TBD"
 has_pip_installed="TBD"
 has_pyserial_installed="TBD"
 comport=$1
@@ -20,17 +20,17 @@ fwdir=firmware/esp32
 
 # check if esptool just runs out of the box
 if $esptool_bin --help 2>/dev/null 1>/dev/null ; then esptool_bin_runs="yes"; else esptool_bin_runs="no"; fi
-if $esptool_py --help 2>/dev/null 1>/dev/null ; then esptool_runs="yes"; else esptool_runs="no"; fi
+if $esptool_py --help 2>/dev/null 1>/dev/null ; then esptool_py_runs="yes"; else esptool_py_runs="no"; fi
 if python -m pip --version 2>/dev/null 1>/dev/null ; then has_pip_installed="yes"; else has_pip_installed="no"; fi
 if python -m serial --help 2>/dev/null 1>/dev/null ; then has_pyserial_installed="yes"; else has_pyserial_installed="no"; fi
 echo
 echo "############################################"
 echo "bundled esptool binary works: " $esptool_bin_runs
-echo "bundled esptool.py works: " $esptool_runs
+echo "bundled esptool.py works: " $esptool_py_runs
 echo "python3-pip installed: " $has_pip_installed
 echo "pyserial installed: " $has_pyserial_installed
 echo
-if [ "$esptool_bin_runs" = "yes" -o "$esptool_runs" = "yes" -o "$has_pip_installed" = "yes" ]; then
+if [ "$esptool_bin_runs" = "yes" -o "$esptool_py_runs" = "yes" -o "$has_pip_installed" = "yes" ]; then
 	echo "Installation requirements fullfilled"
 else
 	echo "Installation requirements not fullfilled"
@@ -74,7 +74,7 @@ esptool="tools/python/bin/esptool.py"
 if [ "$esptool_bin_runs" = "yes" ]; then
         esptool=$esptool_bin;
        	run_esptool;
-elif [ "$esptool_runs" = "yes" ]; then
+elif [ "$esptool_py_runs" = "yes" ]; then
         esptool=$esptool_py;
        	run_esptool;
 elif [ "$has_pip_installed" = "yes" ]; then
